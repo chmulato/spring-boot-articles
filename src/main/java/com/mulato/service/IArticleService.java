b21ac1975b0e4eb867604bfa463a32583740a98b
Christian Mulato <chmulato@gmail.com>
2018-01-27 20:46:01 -0200

    Projeto exemplo de Restful Service

---
+++ src/main/java/com/mulato/service/IArticleService.java
@@ -0,0 +1,13 @@
+package com.mulato.service;
+
+import java.util.List;
+
+import com.mulato.entity.Article;
+
+public interface IArticleService {
+     List<Article> getAllArticles();
+     Article getArticleById(int articleId);
+     boolean addArticle(Article article);
+     void updateArticle(Article article);
+     void deleteArticle(int articleId);
+}
